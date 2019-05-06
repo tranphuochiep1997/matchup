c177eabc4f2efbc4826fde13eb33c965752e91dd
Tran Tien Hiep <tienhieptranuiu@gmail.com>
2019-05-06 22:27:47 +0700

    add Home view

---
+++ form-create-match/header.php
@@ -0,0 +1,10 @@
+<?php
+echo '<div class="fixed-header">';
+echo    '<nav>';
+echo        '<span style="float: left; padding: 7px 25px;">MATCH MAKING</span>';
+echo        '<a href="#">Login</a>';
+echo        '<a href="#">Create Match</a>';
+echo        '<a href="#">My Matches</a>';
+echo    '</nav>';
+echo '</div>';
+?>
--- sql.sql
+++ sql.sql
@@ -1,14 +1,30 @@
-CREATE DATABASE matchup CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CREATE DATABASE matchup
+CHARACTER
+SET utf8mb4
+COLLATE utf8mb4_unicode_ci;
 
 use matchup;
 
-CREATE TABLE matches (
-  id int NOT NULL AUTO_INCREMENT,
-  title varchar(255),
+CREATE TABLE matches
+(
+  id int NOT NULL
+  AUTO_INCREMENT,
+  title varchar
+  (255),
   kind int default 0,
   startTime datetime,
   scoreA int,
   scoreB int,
   status int,
-  PRIMARY KEY(id)
+  loc varchar
+  (255),
+  size INT,
+  PRIMARY KEY
+  (id)
 );
+
+  INSERT INTO matches
+    (title, startTime, loc, size)
+  VALUES
+    ('Friendly', Now(), 'Chi Lang', 10)
+
