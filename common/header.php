147863171b0cd037d65b602c82c000161b2d4865
vntdinh <thedynamite97@gmail.com>
2019-05-10 12:20:53 +0700

    fix header

--- common/header.php
+++ common/header.php
@@ -6,10 +6,14 @@ echo        '<a href="/matchup/home" style="float:left">MATCH MAKING</a>';
 				echo '<a href = "/matchup/auth/logout.php">Logout</a>';
 				require_once "../dbConfig.php";
 				$player_id = $_COOKIE["player_id"];
-				$sql = "SELECT * FROM player where player_id = '$player_id'";
+				$sql = "SELECT p.name FROM player p where p.player_id = '$player_id'";
 				$result = mysqli_query($link, $sql);
-				$playerName = mysql_result($result,'name');				
-				echo '<a>Welcome ' . $playerName . '</a>';
+				if(mysqli_num_rows($result) > 0){
+					while($row = mysqli_fetch_array($result)){ 
+						echo '<a>Welcome ' . $row['name'] . '</a>';
+					}
+				}			
+				
 			}
 			else {
 				echo '<a href = "/matchup/auth/login.php">Login</a>';
